65db707e2db6712e91558d339d931fe6cfeae5e6
trust musikiri <49308670+trustwebmaster@users.noreply.github.com>
2024-07-18 22:51:14 +0200

    feature : added spatie roles

--- app/Models/User.php
+++ app/Models/User.php
@@ -7,10 +7,11 @@ use App\Traits\HasUid;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Spatie\Permission\Traits\HasRoles;
 
 class User extends Authenticatable
 {
-    use HasFactory, Notifiable , HasUid;
+    use HasFactory, Notifiable , HasUid , HasRoles;
 
     /**
      * The attributes that are mass assignable.
