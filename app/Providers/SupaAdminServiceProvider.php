7d125db743a63699b6dfa298a7e31d10f1452ec4
trust musikiri <49308670+trustwebmaster@users.noreply.github.com>
2024-07-20 23:30:48 +0200

    feature : added service provider for super admin

---
+++ app/Providers/SupaAdminServiceProvider.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Providers;
+
+use Illuminate\Support\Facades\Gate;
+use Illuminate\Support\ServiceProvider;
+
+class SupaAdminServiceProvider extends ServiceProvider
+{
+    /**
+     * Register services.
+     */
+    public function register(): void
+    {
+        //
+    }
+
+    /**
+     * Bootstrap services.
+     */
+    public function boot(): void
+    {
+                Gate::before(function ($user, $ability) {
+                         if ($user->hasRole('Super-Admin')) {
+                                 return true;
+                        }
+                });
+    }
+}
